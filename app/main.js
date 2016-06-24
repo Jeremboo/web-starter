312d25580b3bdfba1c0150d7f4cacf5b5d3d0f37
jeremie.boulay <jeremie.boulay@astrium-eu-projects.net>
2016-06-24 12:42:48 +0200

    fix(font): font written with mixin

--- app/main.js
+++ app/main.js
@@ -11,6 +11,7 @@ import ReactDOM from 'react-dom';
 import Root from './router';
 
 import './style/base.styl';
+import './style/fonts.styl';
 
 // Enable react dev-tools (https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi)
 window.React = React;
