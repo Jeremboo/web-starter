7735276b9134080f8e1f05749aa64bc17bdf91d6
Jérémie Boulay <jeremie.boulay@inovia-team.com>
2016-09-09 12:27:35 +0200

    :gem: add redux and redux-router

--- app/main.js
+++ app/main.js
@@ -7,13 +7,47 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { createHistory } from 'history';
 
-import Root from './router';
+import reducers from 'core/reducers';
 
 import './style/base.styl';
 import './style/fonts.styl';
 
+import Layout from 'views/Layout';
+import Home from 'views/Home';
+
+
 // Enable react dev-tools (https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi)
 window.React = React;
 
-ReactDOM.render(<Root />, app);
+const store = createStore(
+  combineReducers(Object.assign({
+    reducers,
+    routing: routerReducer,
+  }))
+);
+
+// TODO to testing
+const history = syncHistoryWithStore(
+  useRouterHistory(createHistory)({
+    basename: window.location.pathname,
+  }),
+  store
+);
+
+ReactDOM.render(
+  <Provider store={store} >
+    <Router history={history}>
+      <Route path="/" component={Layout} >
+        <IndexRoute component={Home} />
+        <Route path="/home" component={Home} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
