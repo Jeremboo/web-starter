56174b90141d74f1b36103ccd3d2b5f91f291a2d
Jérémie Boulay <jeremie.boulay@inovia-team.com>
2016-09-09 18:17:30 +0200

    :art: new nommage to views

--- app/main.js
+++ app/main.js
@@ -18,8 +18,8 @@ import './style/base.styl';
 import './style/fonts.styl';
 
 import Layout from 'views/Layout';
-import Home from 'views/Home';
-import TodoApp from 'views/TodoApp';
+import HomeView from 'views/HomeView';
+import TodoAppView from 'views/TodoAppView';
 
 
 // Enable react dev-tools (https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi)
@@ -44,8 +44,8 @@ ReactDOM.render(
     <Router history={history}>
       <Route path="/" component={Layout} >
         <IndexRoute component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/todo" component={TodoApp} />
+        <Route path="/home" component={HomeView} />
+        <Route path="/todo" component={TodoAppView} />
       </Route>
     </Router>
   </Provider>,
