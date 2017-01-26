e6957a1f64f83a73891fcf01826d5f15a3cd10f2
Jérémie Boulay <boulayj@proximity.bbdo.fr>
2017-01-26 11:59:34 +0100

    :gem: add DevTools and helpers to toogle him

--- app/main.js
+++ app/main.js
@@ -12,7 +12,4 @@ import Root from 'core/router';
 
 import 'style/base.styl';
 
-// Enable react dev-tools (https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi)
-window.React = React;
-
 ReactDOM.render(<Root />, app);
