5975cfc2d96b586ceb9782e2d1e9a4894217bc93
Jérémie Boulay <jeremi.boulay@gmail.com>
2017-07-19 10:32:03 +0200

    :bug: fix mobx implementation

--- app/core/router.js
+++ app/core/router.js
@@ -1,10 +1,12 @@
-
 import React from 'react';
 import { Router, Route } from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
+import { Provider } from 'mobx-react';
 
 import Layout from 'views/Layout';
 import Home from 'views/Home';
+
+import store from 'store';
 
 const history = createBrowserHistory({
   basename: process.env.BASENAME.length > 0 ? process.env.BASENAME : window.location.pathname,
@@ -17,12 +19,14 @@ export const openPath = (path) => {
 };
 
 const Root = () => (
-  <Router history={history}>
-    <Layout>
-      <Route path="/" component={Home} />
-      <Route path="/home" component={Home} />
-    </Layout>
-  </Router>
+  <Provider {...store} >
+    <Router history={history}>
+      <Layout>
+        <Route path="/" component={Home} />
+        <Route path="/home" component={Home} />
+      </Layout>
+    </Router>
+  </Provider>
 );
 
 export default Root;
